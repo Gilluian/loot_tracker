f506955043c2af2535231bf9145ece10b8ab0666
Alexander Hall <alexander.hall@lpcnextlight.com>
2026-05-08 19:01:18 -0600

    created the db instance and successfully invoked.

--- db_funcs.py
+++ db_funcs.py
@@ -1,3 +1,18 @@
-
 import psycopg2
 
+
+class LootTracker:
+    def __init__(self, db_config):
+        self.db_config = db_config
+        self.conn = None
+
+        try:
+            self.conn = psycopg2.connect(**self.db_config)
+            print("Database connection established.")
+        except Exception as e:
+            print(f"Error connecting to database: {e}")
+
+    def __del__(self):
+        if self.conn:
+            self.conn.close()
+            print("Database connection closed.")
--- main.py
+++ main.py
@@ -1,22 +1,38 @@
+import os
+from dotenv import load_dotenv
+from db_funcs import LootTracker as lt
 
+load_dotenv()
 
 def main():
+    db_config = {
+        'dbname': os.getenv('DB_NAME'),
+        'user': os.getenv('DB_USER'),
+        'password': os.getenv('DB_PASSWORD'),
+        'host': os.getenv('DB_HOST'),
+        'port': os.getenv('DB_PORT')
+    }
+    db = lt(db_config)
+
+
+
     startup()
     run_program()
-    cleanup()
+    cleanup(db)
 
 def startup():
-    # TODO [LOOT-20]: What actions should be taken when the program starts?
+
     print("Starting the program...")
 
 
-def run_program():    # TODO [LOOT-21]: What is the main logic of the program?
+def run_program():
     print('doing the things...')
 
     
-def cleanup():
-    # TODO [LOOT-22]: What actions should be taken when the program ends?
+def cleanup(db):
+    del db
     print("Cleaning up...") 
+    
 
     
 if __name__ == "__main__":
